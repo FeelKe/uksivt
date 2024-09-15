0656054f66fef0b307cd3da6faaadff428ef5b32
FeelKe <felep.jopa@mail.ru>
2024-09-15 21:25:26 +0500

    Your commit message

---
+++ mainapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def account(request):
+    return render(request, 'account.html')
+
+
+def list(request):
+    return render(request, 'list.html')
