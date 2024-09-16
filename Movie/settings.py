b0a66741f32cf00c6cc241fb4362b1a9e4e91a6e
FeelKe <felep.jopa@mail.ru>
2024-09-16 23:51:38 +0500

    railway

--- Movie/settings.py
+++ Movie/settings.py
@@ -74,7 +74,6 @@ WSGI_APPLICATION = 'Movie.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
