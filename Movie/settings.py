7d6b936341d417fb2840b95c93db8a558d3bcb60
FeelKe <felep.jopa@mail.ru>
2024-09-16 23:51:19 +0500

    railway

--- Movie/settings.py
+++ Movie/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'Movie.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
